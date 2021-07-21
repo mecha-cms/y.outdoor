b9c840736a0a37afeb0c1b4d74828dc33f070aa4
Taufik Nurrohman <t.nurrohman77@gmail.com>
2021-07-21 23:06:35 +0700

    Add More Extension Missing Warn

--- widget/page/popular.php
+++ widget/page/popular.php
@@ -2,7 +2,7 @@
     'title' => $title ?? i('Popular %s', ['Posts']),
     'take' => $take ?? 5,
     'sort' => [-1, 'view']
-]) : self::widget('page', [
+]) : self::widget([
     'title' => $title ?? i('Popular %s', ['Posts']),
     'content' => '<p>' . i('Missing %s extension.', ['<a href="https://mecha-cms.com/store/extension/view" target="_blank">view</a>']) . '</p>'
 ]);
